f68addbe973c0aada4753ca0548813466bd5882c
the-greenwell <ant.greenwell2@gmail.com>
2022-03-19 22:22:14 -0700

    finished validation on post and put routes, login logic finished, user able to update poll's format

--- routes.js
+++ routes.js
@@ -1,33 +1,92 @@
 const express = require('express');
 const router = express.Router();
-const ipInt = require('ip-to-int');
 
-const { Option, Poll } = require('./Models/Poll')
+const { body, header, validationResult } = require('express-validator');
+// const ipInt = require('ip-to-int');
+const bcrypt = require('bcryptjs');
+
+const jwt = require('jsonwebtoken');
+
+const { Option, Poll } = require('./Models/Poll');
+const { verifyToken } = require('./config');
+
 const Voter = require('./Models/Voter');
 
-const { body, check, validationResult } = require('express-validator');
 
 //
 // UPDATE VOTE ROUTE TO CHECK IP'S
 //
-// VALIDATION TO DO - ALL
 
 router.post('/newpoll',
-  (req,res) => {
-    const dbPoll = new Poll(req.body);
-    dbPoll.save()
-      .then((poll)=>{
-        res.status(200).json({ poll: poll })
-      })
-      .catch((err)=>{
-        res.status(400).json({ err: err })
-      });
+    body('subject').isString().notEmpty().escape().trim(),
+    body('author').isString().notEmpty().escape().trim().toUpperCase(),
+    body('password').isString().notEmpty(),
+    body('format').isString().optional().isIn(['default','bar','pie','radar']),
+    body('options').isArray({ min:2 }),
+    body('options.*.description').isString().notEmpty().trim(),
+    async (req,res) => {
+      const errors = validationResult(req);
+      if(!errors.isEmpty()){
+        return res.status(400).json({error: errors.array()});
+      }
+
+      const pollBody = req.body;
+      pollBody.password = await bcrypt.hash(req.body.password, 10);
+
+      const dbPoll = new Poll(pollBody);
+      dbPoll.save()
+        .then((poll)=>{
+          return res.status(200).json({ poll: poll })
+        })
+        .catch((err)=>{
+          return res.status(400).json({ err: err })
+        });
   }
 )
 router.get('/:id',
   async (req,res) => {
       const poll = await Poll.findById(req.params.id, {password:0}).populate('options');
-      res.json({poll:poll})
+      return res.status(200).json({poll:poll})
+  }
+)
+router.put('/:id',
+  header('x-access-token').isJWT().withMessage('Unauthorized Access'),
+  verifyToken,
+  body('format').isString().notEmpty().isIn(['default','bar','pie','radar']),
+  (req,res) => {
+    Poll.findByIdAndUpdate(req.params.id,{
+      '$set' : { 'format' : req.body.format }
+    }, {new: true}).then((poll) => {
+      return res.status(200).json({poll: poll})
+    }).catch((err) => {
+      return res.status(400).json({err: err})
+    })
+  }
+)
+router.post('/:id/login',
+  (req,res) => {
+    Poll.findById(req.params.id, (err,poll) => {
+      if(err) return res.status(400).json({error: err,location:'findBy'})
+      bcrypt.compare(req.body.password, poll.password)
+        .then(() => {
+          const payload = { isLoggedIn: true }
+          jwt.sign(
+            payload,
+            process.env.JWT_SECRET,
+            {expiresIn:86400},
+            (err,token) => {
+              if(err) return res.status(400).json({error: err,location:'jwt'})
+              return res.status(200).json({
+                token: token,
+                exp: 86400
+              })
+            }
+          )
+        })
+        .catch((err) => {
+          return res.status(400).json({error: err,location:'bcrypt'})
+        })
+    })
   }
 )
 router.patch('/:id/:option',
@@ -57,14 +116,5 @@ router.patch('/:id/:option',
       })
   }
 )
-router.put('/:id', (req,res) => {
-  Poll.findByIdAndUpdate(req.params.id,{
-    '$set' : { 'format' : req.body.format }
-  }, {new: true}).then((poll) => {
-    res.status(200).json({poll: poll})
-  }).catch((err) => {
-    res.status(400).json({err: err})
-  })
-})
 
 module.exports = router;
